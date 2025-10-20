ebabb3f26e0ecf4ed5699aa7c64908d62cbe9d39
Prakash-sa <sainiprakash525@gmail.com>
2025-10-20 00:56:43 -0600

    added

---
+++ questions/parking lot/ParkingTicket.py
@@ -0,0 +1,12 @@
+class ParkingTicket:
+    def __init__(self):
+        self.ticket_no = None
+        self.entry_time = None
+        self.exit_time = None
+        self.amount = None
+        self.status = None 
+
+        self.vehicle = None
+        self.payment = None 
+        self.entrance = None
+        self.exit_ins = None
